dae313cccfed38082a0c8dcab43576a2b5e5d80e
frame-1 <muthee031@gmail.com>
2022-04-19 09:43:03 -0700

    Task 0

--- print_chr.c
+++ print_chr.c
@@ -1,14 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_c - A function that prints a char
  * @c: character to print
  * Return: Always 1 (Success)
  */
 
-int print_c(va_list c)
+int print_c(va_list arguments, char *buf, unsigned int ibuf)
 {
-	char character = (char)va_arg(c, int);
+	char c;
 
-	_putchar(character);
+	c = va_arg(arguments, int);
+	handl_buf(buf, c, ibuf);
+
 	return (1);
 }

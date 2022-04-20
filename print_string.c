3ac0a95bf9f8b1a0dd65c08e8ead700b50424b33
Kaingusaid <kaingusaid22@gmail.com>
2022-04-19 19:06:48 -0700

    string

--- print_string.c
+++ print_string.c
@@ -1,34 +1,79 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * print_S - A function that prints a string and nonprintable
- * character ascii values
- * @S: string to print
- * Return: number of printed characters
+ * print_s - prints a string
+ * @args: args
+ * Return: i number of characters printed
  */
 
-int print_S(va_list S)
+int print_s(va_list args)
 {
-	unsigned int i = 0;
-	int counter = 0;
-	char *str = va_arg(S, char *);
+	int i;
+	char *str;
+
+	str = va_arg(args, char *);
 
 	if (str == NULL)
 		str = "(null)";
-	for (; str[i]; i++)
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
+
+/**
+ * find_hex - find the hexadecimal expression for non printable characters
+ * @c: non printable char
+ * Return: i
+ */
+int find_hex(char c)
+{
+	int i;
+	char dif = 7;
+	char hex[2];
+
+	hex[0] = c / 16;
+	hex[1] = c % 16;
+	for (i = 0; i < 2; i++)
 	{
-		if (str[i] < 32 || str[i] >= 127)
+		if (hex[i] >= 10)
+			_putchar('0' + dif + hex[i]);
+		else
+			_putchar('0' + hex[i]);
+	}
+	return (i);
+}
+
+/**
+ * print_S - prints a string and non printable characters
+ * are changed for \x
+ * @args: args
+ * Return: len
+ */
+
+int print_S(va_list args)
+{
+	char *str;
+	int i, len = 0;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] > 0 && str[i] < 32) || (str[i] >= 127))
 		{
 			_putchar('\\');
 			_putchar('x');
-			counter += 2;
-			counter += print_x(S);
+			len += 2 + find_hex(str[i]);
 		}
 		else
 		{
 			_putchar(str[i]);
-			counter++;
+			len++;
 		}
-	}
-	return (counter);
+	} /* end for */
+	return (len);
 }
